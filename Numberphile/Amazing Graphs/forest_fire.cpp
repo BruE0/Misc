294f7d54c9510216917eb7f25a9914b4e7d93bc2
BruE0 <47761923+BruE0@users.noreply.github.com>
2019-08-15 18:48:26 -0300

    Update forest_fire.cpp

--- Numberphile/Amazing Graphs/forest_fire.cpp
+++ Numberphile/Amazing Graphs/forest_fire.cpp
@@ -1,7 +1,12 @@
 /*
-C++
-forest_fire.cpp
-2019.08
+    C++
+    forest_fire.cpp
+    2019.08
+    https://oeis.org/A229037
+Sequence of positive integers where each is chosen 
+to be as small as possible subject to the condition that 
+no three terms a(j), a(j+k), a(j+2k) (for any j and k) 
+form an arithmetic progression.
 */
 
 
@@ -10,10 +15,10 @@ forest_fire.cpp
 #include <array>
 
 
-const int TOTAL = 20000;
+const int NO_POINTS = 100;
 
 
-bool three_points_same_distance(std::array<int, TOTAL> nums, int X) {
+bool three_points_same_distance(std::array<int, NO_POINTS> nums, int X) {
     for (int x=X-1; x>=X/2; --x) {
         if ((2*nums[x] - nums[X]) == nums[2*x - X]) {
             return true;
@@ -23,10 +28,10 @@ bool three_points_same_distance(std::array<int, TOTAL> nums, int X) {
 }
 
 
-std::array<int, TOTAL> forest_fire() {
-    std::array<int, TOTAL> nums = {1,1};
+std::array<int, NO_POINTS> forest_fire() {
+    std::array<int, NO_POINTS> nums = {1,1};
     int attempt;
-    for (int X=2; X<TOTAL; ++X) {
+    for (int X=2; X<NO_POINTS; ++X) {
         std::cout << X << "\n";
         attempt = 1;
         while(1) {
@@ -43,17 +48,19 @@ std::array<int, TOTAL> forest_fire() {
 
 
 int main() {
-    std::array<int, TOTAL> nums;
+    std::array<int, NO_POINTS> nums;
     nums = forest_fire();
 
-    std::cout << "Writing to file" << "\n";
+    std::cout << "Writing to file \'test.txt\'" << "\n";
     std::ofstream my_file;
+
     my_file.open("test.txt");
     my_file << "[";
-    for (int i=0; i<TOTAL-1; i++) {
+    for (int i=0; i<NO_POINTS-1; i++) {
         my_file << nums[i] << ", ";
     }
-    my_file << nums[TOTAL-1] << "]" << "\n";
+    my_file << nums[NO_POINTS-1] << "]" << "\n";
     my_file.close();
+    
     return 0;
 }
