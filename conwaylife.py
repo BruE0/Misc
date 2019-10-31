6e9265f78aff9bf4d3d180a7d3ebb9f069c837c5
BruE0 <47761923+BruE0@users.noreply.github.com>
2019-10-31 07:07:04 -0300

    Update conwaylife.py

--- conwaylife.py
+++ conwaylife.py
@@ -1,54 +1,48 @@
 #!/usr/bin/env python3
 """
     conwaylife.py
-    2018
-    simple console based game of life using numpy.
+    2019
+    game of life using numpy/scipy.
 
     - loads a starting grid from txt file.
     - wait for input:
       > empty line to advance a certain step(default=1) of generations.
       > writing a number changes step.
+
 """
 
 
 import numpy as np
+from scipy.ndimage import convolve
 import os
 
 
-CLEAR_CONSOLE = "cls" if os.name == "nt" else "clear"
+CLEAR = "cls" if os.name == "nt" else "clear"
+NEIGHBOR_KERNEL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
 
-def count_around(nparray):
-    '''Counts the number of cells alive around each cell.'''
-    nparray = (np.roll(nparray, 1, axis=0) + np.roll(nparray, -1, axis=0) +
-        np.roll(nparray, 1, axis=1) + np.roll(nparray, -1, axis=1) +
-        np.roll(nparray, (1, 1), axis=(0, 1)) + np.roll(nparray, (-1, 1), axis=(0, 1)) +
-        np.roll(nparray, (1, -1), axis=(0, 1)) + np.roll(nparray, (-1, -1), axis=(0, 1)) )
-    nparray = np.pad(nparray[1:-1,1:-1], pad_width=1, mode='constant', constant_values=0)
-    return nparray
-
-
-def live_or_die(nparray, count_around):
-    '''Uses Conway's standard rules to check if a cell will live or die.'''
-    nparray = np.where(nparray == 1, np.where((count_around == 2) | (
-        count_around == 3), 1, 0), np.where(count_around == 3, 1, 0))
+def live_or_die(nparray):
+    count_around = convolve(nparray, NEIGHBOR_KERNEL, mode="constant")
+    nparray = np.where(
+        nparray == 1,
+        np.where((count_around == 2) | (count_around == 3), 1, 0),
+        np.where(count_around == 3, 1, 0),
+    )
     return nparray
 
 
 def drawboard(nparray):
     array_draw = np.where(nparray == 1, "#", " ")
-    print("\n".join([''.join(row) for row in array_draw]))
+    print(*["".join(row) for row in array_draw], sep="\n")
 
 
-def loadboard(filename, live="1"):
+def loadboard(filename, live):
     with open(filename, "rb") as f:
         data = f.read().splitlines()
 
-    nparray = np.vstack([np.frombuffer(line, dtype=np.uint8) for line in data])
+    nparray = np.vstack(np.frombuffer(line, dtype=np.uint8) for line in data)
     nparray = np.where(nparray == ord(live), 1, 0)
-    nparray = np.pad(nparray, pad_width=1, mode='constant', constant_values=0)
     return nparray
-
 
 
 def main():
@@ -57,14 +51,13 @@ def main():
     step = 1
 
     while True:
-        os.system(CLEAR_CONSOLE)
+        os.system(CLEAR)
 
         for _ in range(step):
-            around = count_around(house)
-            house[1:-1,1:-1] = live_or_die(house[1:-1,1:-1],around[1:-1,1:-1])
+            house = live_or_die(house)
             generation += 1
 
-        drawboard(house[1:-1,1:-1])
+        drawboard(house)
 
         answer = input(f"{generation:06d}>")
         if answer.isdigit():
