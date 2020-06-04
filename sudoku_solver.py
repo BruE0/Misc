02d031365baa6e861b1c9fefc9789d58a37d25d9
BruE0 <47761923+BruE0@users.noreply.github.com>
2020-06-04 15:30:11 -0300

    Cleanup

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -16,7 +16,7 @@ class Board:
         chars = []
         for i in range(9):
             for j in range(9):
-                item = self._data[i*9+j]
+                item = self[i,j]
                 chars.append(f"{item}" if item is not None else '_')
                 chars.append('_' if i%3 == 2 else ' ')
                 if j%3 == 2:
@@ -29,8 +29,9 @@ class Board:
 
     def __setitem__(self, coord, value):
         index = coord[0]*9+coord[1]
-        if index not in self._static_digits_index:
-            self._data[index] = value
+        if index in self._static_digits_index:
+            raise ValueError("Cannot change number an initial number.")
+        self._data[index] = value
 
 
     def is_available(self, coord):
@@ -38,7 +39,7 @@ class Board:
         return coord[0]*9+coord[1] not in self._static_digits_index
 
     def get_digits_available(self, coord):
-        """ Returns all the digits available for the coord, considering sudoku's rules """
+        """ Returns all the digits available for the coord, considering sudoku's rules. """
         ii, jj = coord
         block_start_i = (ii//3)*3 # rounds ii down to either 0, 3 or 6
         block_start_j = (jj//3)*3 # rounds jj down to either 0, 3 or 6
@@ -55,8 +56,8 @@ class Board:
 
 def next_coord(coord):
     """ Returns `next` coordinate (i, j) in grid. 
-            (... -> (1,8) -> (2, 0) -> (2, 1) -> ...)
-        If coord is the last one, it returns (9, 0) as opposed to wrapping around.
+            (... -> (1, 8) -> (2, 0) -> (2, 1) -> ...)
+        If coord is the last one, it returns (9, 0) as opposed to wrapping around to (0, 0).
     """
     return (coord[0] + (coord[1]==8), (coord[1] + 1) % 9)
 
@@ -68,17 +69,13 @@ def recursion_solve(board, coord=(0, 0)):
 
     if board.is_available(coord):
         nums = board.get_digits_available(coord)
-        if not nums:
-            return False
-        nums = sorted(nums, reverse=True)
-        while True:
+        while nums:
             board[coord] = nums.pop()
             result = recursion_solve(board, next_coord(coord))
             if result:
                 return True
-            if not nums:
-                board[coord] = None
-                return False
+        board[coord] = None
+        return False
     else:
         return recursion_solve(board, next_coord(coord))
 
