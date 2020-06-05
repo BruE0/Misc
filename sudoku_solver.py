d73b75f7289c845e0386c0fd5822af0831f8aa83
BruE0 <47761923+BruE0@users.noreply.github.com>
2020-06-05 13:10:00 -0300

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,20 +3,26 @@
 """
     sudoku_solver.py
     2020
+    Uses a simple brute-force backtracking algorithm.
 """
 
 
 
 class Board:
     def __init__(self, data):
-        self._data = [v for row in data for v in row]
-        self._static_digits_index = {i for i,v in enumerate(self._data) if v is not None}
+        self._data = data
+        self._static_digits_index = {
+            (i, j)
+            for i, row in enumerate(data)
+            for j, element in enumerate(row)
+            if element is not None
+        }
 
     def __str__(self):
         chars = []
         for i in range(9):
             for j in range(9):
-                item = self[i,j]
+                item = self._data[i][j]
                 chars.append(f"{item}" if item is not None else '_')
                 chars.append('_' if i%3 == 2 else ' ')
                 if j%3 == 2:
@@ -25,18 +31,17 @@ class Board:
         return ''.join(chars)
 
     def __getitem__(self, coord):
-        return self._data[coord[0]*9+coord[1]]
+        return self._data[coord[0]][coord[1]]
 
     def __setitem__(self, coord, value):
-        index = coord[0]*9+coord[1]
-        if index in self._static_digits_index:
-            raise ValueError("Cannot change number an initial number.")
-        self._data[index] = value
-
+        if self.is_available(coord):
+            self._data[coord[0]][coord[1]] = value
+        else:
+            raise IndexError("Cannot assign numbers to coordinates with initial values.")
 
     def is_available(self, coord):
         """ Returns true if the coordinate is a valid slot (not one of the initial digits). """
-        return coord[0]*9+coord[1] not in self._static_digits_index
+        return coord not in self._static_digits_index
 
     def get_digits_available(self, coord):
         """ Returns all the digits available for the coord, considering sudoku's rules. """
@@ -44,12 +49,12 @@ class Board:
         block_start_i = (ii//3)*3 # rounds ii down to either 0, 3 or 6
         block_start_j = (jj//3)*3 # rounds jj down to either 0, 3 or 6
         digits_in_block = set(
-            self[y,x]
+            self._data[y][x]
             for y in range(block_start_i, block_start_i + 3)
             for x in range(block_start_j, block_start_j + 3)
         )
-        digits_in_row = set(self[ii,x] for x in range(9))
-        digits_in_column = set(self[y,jj] for y in range(9))
+        digits_in_row = set(self._data[ii][x] for x in range(9))
+        digits_in_column = set(self._data[y][jj] for y in range(9))
         return set(range(1, 10)) - (digits_in_block | digits_in_column | digits_in_row)
 
 
@@ -57,13 +62,13 @@ class Board:
 def next_coord(coord):
     """ Returns `next` coordinate (i, j) in grid. 
             (... -> (1, 8) -> (2, 0) -> (2, 1) -> ...)
-        If coord is the last one, it returns (9, 0) as opposed to wrapping around to (0, 0).
+        If coord is last (8, 8), it returns (9, 0) as opposed to wrapping around to (0, 0).
     """
     return (coord[0] + (coord[1]==8), (coord[1] + 1) % 9)
 
 
 def recursion_solve(board, coord=(0, 0)):
-    """ Solves sudoku board using brute-force backtracking algorithm """
+    """ Solves sudoku board using brute-force backtracking algorithm. """
     if coord == (9, 0):
         return True
 
@@ -91,7 +96,6 @@ def main():
              [None,None,None,None,None,None,None,8,None],
              [None,3,None,6,None,None,1,5,7],
              [None,None,None,8,1,None,None,None,6]]
-
 
     board = Board(test1)
 
